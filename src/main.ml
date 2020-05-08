0409f2eabe661a581baeee263dd780dbfe60abf4
Brian Jones <beeceej.code@gmail.com>
2020-05-08 14:34:40 -0400

    action: Project scaffolding
    load in the GITHUB_EVENT_PATH environment variable, read the file in

---
+++ src/main.ml
@@ -0,0 +1,19 @@
+module Github = struct
+  let token = Sys.getenv "GH_TOKEN"
+  let headers = Cohttp.Header.init_with "Authorization" ("token " ^ token)
+
+  let load_event =
+    let ch = Sys.getenv "GITHUB_EVENT_PATH" |> open_in  in
+    let s = really_input_string ch (in_channel_length ch) in
+    let () = close_in ch in
+    let () = print_endline s in
+    Yojson.Basic.from_string s
+end
+
+module Main = struct
+  let run () =
+    Github.load_event
+end
+
+
+let _ = Main.run ()
