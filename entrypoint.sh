d6a453a276e8fe4572b267d6940e50829fc3ba81
beeceej <beeceej@hey.com>
2020-10-23 16:38:45 -0400

    Pass host env vars into container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 latest_tag=$(git tag  | grep -E '^v[0-9]' | sort -V | tail -1)
 latest_tag=latest
 echo -e "FROM beeceej/lgtm:${latest_tag}\nENTRYPOINT [ \"/bin/lgtm\" ]" | docker build --rm -t runtime -
-docker run --rm runtime:latest
+docker run --rm -e runtime:latest
