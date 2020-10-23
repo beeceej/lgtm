10a238b00e4e0302c2e1421d9f39a78b23c9eff6
beeceej <beeceej@hey.com>
2020-10-23 17:30:28 -0400

    Add missing env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 latest_tag=$(git tag  | grep -E '^v[0-9]' | sort -V | tail -1)
 latest_tag=latest
 echo -e "FROM beeceej/lgtm:${latest_tag}\nADD . .\nENTRYPOINT [ \"/bin/lgtm\" ]" | docker build --rm -t runtime -
-docker run --rm -e GH_TOKEN -e  runtime:latest
+docker run --rm -e GH_TOKEN -e GITHUB_EVENT_PATH runtime:latest
