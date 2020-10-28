38f727e26eb9117c4811337195c85329689468df
beeceej <beeceej@hey.com>
2020-10-27 20:59:20 -0400

    Name file event.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 latest_tag=$(git tag  | grep -E '^v[0-9]' | sort -V | tail -1)
 latest_tag=latest
 pwd
-cp "$GITHUB_EVENT_PATH" .
+cp "$GITHUB_EVENT_PATH" event.json
+
 echo -e "FROM beeceej/lgtm:${latest_tag}\nADD . .\nENTRYPOINT [ \"/bin/lgtm\" ]" | docker build --rm -t runtime -
 docker run --rm -e GH_TOKEN -e GITHUB_EVENT_PATH=event.json runtime:latest
