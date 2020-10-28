355f6f7daf70b5196c8ee83dce17f56bd9d2677f
beeceej <beeceej@hey.com>
2020-10-27 20:51:13 -0400

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 latest_tag=$(git tag  | grep -E '^v[0-9]' | sort -V | tail -1)
 latest_tag=latest
-echo -e "FROM beeceej/lgtm:${latest_tag}\nADD . .\nENTRYPOINT [ \"/bin/lgtm\" ]" | docker build --rm -t runtime -
+echo -e "FROM beeceej/lgtm:${latest_tag}\nADD . .\nENTRYPOINT [ \"ls -lah\" ]" | docker build --rm -t runtime -
 docker run --rm -e GH_TOKEN -e GITHUB_EVENT_PATH runtime:latest
