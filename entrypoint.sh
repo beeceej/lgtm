4ce7cc2f4fab2856d6c191d24e30d9082a5b9096
beeceej <beeceej@hey.com>
2020-10-27 21:02:27 -0400

    Use relative path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cp "$GITHUB_EVENT_PATH" event.json
 ls -lah
 
 echo -e "FROM beeceej/lgtm:${latest_tag}\nADD . .\nENTRYPOINT [ \"/bin/lgtm\" ]" | docker build --rm -t runtime -
-docker run --rm -e GH_TOKEN -e GITHUB_EVENT_PATH=event.json runtime:latest
+docker run --rm -e GH_TOKEN -e GITHUB_EVENT_PATH="./event.json" runtime:latest
