6928fc5cf39f889b3765816fc2d07d423fa24af7
Brian Jones <brian@beeceej.com>
2023-07-21 23:47:49 -0400

    Debug event.json
    For some reason the action is unable to find it, but it used to be able
    to.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ latest_tag=$(git tag  | grep -E '^v[0-9]' | sort -V | tail -1)
 latest_tag=latest
 pwd
 cp "$GITHUB_EVENT_PATH" event.json
+cat event.json
 ls -lah
 
 echo -e "FROM beeceej/lgtm:${latest_tag}\nADD . .\nENTRYPOINT [ \"/bin/lgtm\" ]" | docker build --rm -t runtime -
